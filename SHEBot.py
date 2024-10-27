7cc5514c3e9db00563c2afeeb140b5ae7aef99d8
chrismalco <chris.malco@outlook.com>
2024-10-27 20:29:02 +0800

    fixed - APIKEY Read and requirements.txt

--- SHEBot.py
+++ SHEBot.py
@@ -10,8 +10,16 @@ import random
 from helper_functions.utility import check_password 
 
 # Load environment variables (OpenAI API key)
-load_dotenv('.env')
-openai.api_key = os.getenv('OPENAI_API_KEY')
+
+if load_dotenv('.env'):
+   # for local development
+   OPENAI_KEY = os.getenv('OPENAI_API_KEY')
+else:
+   OPENAI_KEY = st.secrets['OPENAI_API_KEY']
+
+# Pass the API Key to the OpenAI Client
+client = OpenAI(api_key=OPENAI_KEY)
+# Some other code here are omitted for brevity
 
 # Streamlit UI Setup
 st.set_page_config(page_title="LTA SHEBot", layout="wide")
